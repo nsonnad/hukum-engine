3e57aa1df4350d87484e52abe487e13e1362a196
Nikhil Sonnad <nikhil@nsnnd.com>
2020-06-07 20:55:37 +0530

    make all card ranks atoms instead of mixing int and atom

--- lib/hukum_engine/deck.ex
+++ lib/hukum_engine/deck.ex
@@ -1,13 +1,16 @@
 defmodule HukumEngine.Deck do
   @cards (
     for suit <- [:clubs, :diamonds, :hearts, :spades],
-        rank <- [7, 8, 9, 10, :jack, :queen, :king, :ace],
+        rank <- [:seven, :eight, :nine, :ten, :jack, :queen, :king, :ace],
       do: %{suit: suit, rank: rank}
   )
 
   def shuffled(), do: Enum.shuffle(@cards)
 
-  def value(num) when num in 7..10, do: num
+  def value(:seven), do: 7
+  def value(:eight), do: 8
+  def value(:nine), do: 9
+  def value(:ten), do: 10
   def value(:jack), do: 11
   def value(:queen), do: 12
   def value(:king), do: 13
